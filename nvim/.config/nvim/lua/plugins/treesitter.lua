1d5b54cdec9f133be760faecf9aaa77723039e0d
Orsted <cuong.vm.21@gmail.com>
2025-02-10 13:38:51 +0700

    update tabout plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -18,6 +18,15 @@ return {
       "lua",
       "vim",
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
   },
   config = function(_, opts)
     -- import nvim-treesitter plugin
